11f77c21dc18d5d17ee012da3b7db1ebcf624e8f
licrosea <sevenblock426@gmail.com>
2022-09-18 00:47:40 +0900

    ゲーム初期化ステートを作成（実装はまだ）

--- Assets/ThisProject/Script/GamePlay/GamePlayContext.cs
+++ Assets/ThisProject/Script/GamePlay/GamePlayContext.cs
@@ -6,15 +6,28 @@ public class GamePlayContext : MonoBehaviour
 {
     AState state = null;
 
+    public Field Field { get; private set; } = null;
+    public PieceControll PieceControll { get; private set; } = null;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Field = new Field();
+
+        state = new GameInitState( this );
+        state.Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AState nextState = state.Update();
+
+        if( state != null )
+        {
+            state = nextState;
+            state.Initialize();
+        }
     }
 }
---
+++ Assets/ThisProject/Script/GamePlay/State/GameInitState.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInitState : AState
+{
+    public GameInitState(GamePlayContext owner) : base (owner)
+    {
+
+    }
+
+    public override void Initialize()
+    {
+        // TODO: ƒQ[ƒ€‰Šú‰»ˆ—
+    }
+
+    public override AState Update()
+    {
+        return null;
+    }
+
+}
