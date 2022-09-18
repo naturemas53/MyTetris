eceb46fc8fb265c6efc627fceae213d82a399c71
licrosea <sevenblock426@gmail.com>
2022-09-18 22:05:25 +0900

    ネクスト処理を仮実装

---
+++ Assets/ThisProject/Script/NextPieceSet/SevenBagNextSet.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static CommonDefines;
+
+public class SevenBagNextSet : ANextPieceSet
+{
+    List<EShapeType> inBagPieces = null;
+
+    public SevenBagNextSet()
+    {
+        inBagPieces = new List<EShapeType>();
+        ReloadBag();
+    }
+
+    protected override void AddNextPieceImpl()
+    {
+        int pieceIdx = inBagPieces.Count;
+        EShapeType createShape = inBagPieces[ pieceIdx ];
+
+        // TODO: ピース生成 & 追加処理
+
+        inBagPieces.RemoveAt( pieceIdx );
+
+        if( inBagPieces.Count <= 0 )
+        {
+            ReloadBag();
+        }
+    }
+
+    /// <summary>
+    /// バッグを補充します。
+    /// </summary>
+    void ReloadBag()
+    {
+        for( EShapeType shape = EShapeType.SHAPE_Z; shape < EShapeType.SHAPE_SPECIAL; ++shape )
+        {
+            inBagPieces.Add( shape );
+        }
+    }
+
+}
